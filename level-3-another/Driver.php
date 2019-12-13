8df4e89bdf342f97418f892cb7204cb49745c7f5
Anton Mihaylenko <successfultowork@gmail.com>
2019-12-13 15:35:55 +0200

    level-3-another

---
+++ level-3-another/Driver.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Driver {
+/*это класс водителя мотоцикла, метод take_transport принимает объект (скутераб мотоцикла или гидроцикла)
+затем добавляет в объект имя и в поле transport добавляет весь объект и возвращает его. Метод move берет объект из поля
+move и вызывает его собственный метод move который наследовался от мотомеханики*/
+    public $name;
+    private $transport;
+
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    function take_transport(Mototechnics $mototechnics) { /*Тут полиморфизм*/
+
+        $mototechnics->byname = $this->name;
+        $this->transport = $mototechnics;
+        return $this;
+    }
+
+    function move(Mototechnics $mototechnics){
+        $mototechnics->byname = $this->name;
+		$mototechnics->move();
+    }
+
+    function stop(Mototechnics $mototechnics){
+		$mototechnics->byname = $this->name;
+		$mototechnics->stop();
+    }
+
+}
